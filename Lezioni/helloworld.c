493019775474c37044f83b330009831d9a8c306f
Rhaegal222 <rhaegal222@gmail.com>
2022-03-16 09:43:07 +0100

    commit

---
+++ Lezioni/helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* hello(void* arg){
+    printf("Ciao\n"),
+    return NULL,
+}
+
+int main() {
+    pthread_t tid;
+    pthread_create(&tid, NULL, &hello, NULL);
+
+}
