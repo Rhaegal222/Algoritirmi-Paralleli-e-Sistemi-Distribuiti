a01f7efc07e140d01258c637a7b818056a6321ba
rhaegal222@gmail.com <rhaegal222@gmail.com>
2022-03-16 11:29:25 +0100

    commit

--- Lezioni/helloworld.c
+++ Lezioni/helloworld.c
@@ -4,13 +4,28 @@
 #include <pthread.h>
 
 void* hello(void* arg){
-    printf("Ciao\n");
+    sleep(1);
+    printf("Ciao %d\n", *(int *)arg);
+    delete (int *)arg;
     return NULL;
 }
 
-int main() {
+int main1() {
     pthread_t tid;
     pthread_create(&tid, NULL, &hello, NULL);
-    pthread_join(tid, NULL)
+    pthread_join(tid, NULL);
+    return 0;
+}
 
+int main() {
+    pthread_t tid[10];
+    for (int i=0; i<10; i++){
+        int *p = new int;
+        *p=i;
+        pthread_create(&tid[i], NULL, &hello, p);
+    }
+    for (int i=0; i<10; i++){
+        pthread_join(tid[i], NULL);
+    }
+    return 0;
 }
