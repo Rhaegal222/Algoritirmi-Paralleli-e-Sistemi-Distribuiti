004ede54ed377c84245bdbec480cd171a6f252c2
Rhaegal222 <rhaegal222@gmail.com>
2023-08-29 20:34:09 +0200

    update Barrier.cpp and Ordered_Barrier.cpp

--- Laboratorio/OpenMP/Codici/Barrier.cpp
+++ Laboratorio/OpenMP/Codici/Barrier.cpp
@@ -1,21 +1,51 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+#include <unistd.h>
+
+#define maxsize 3000
+
+MPI_Request send_reqs[maxsize], recv_reqs[maxsize];
+MPI_Status send_statuses[maxsize], recv_statuses[maxsize];
+int cont, send_buffer[maxsize], recv_buffer[maxsize];
+
+void barrier(int rank, int size) {
+    cont++;
+
+    // Invia token a tutti gli altri processi
+    for (int dest = 0; dest < size; dest++) {
+        if (dest != rank) {
+            MPI_Isend(&send_buffer[0], 1, MPI_INT, dest, cont, MPI_COMM_WORLD, &send_reqs[dest]);
+        }
+    }
+
+    // Ricevi token da tutti gli altri processi
+    for (int src = 0; src < size; src++) {
+        if (src != rank) {
+            MPI_Irecv(&recv_buffer[src], 1, MPI_INT, src, cont, MPI_COMM_WORLD, &recv_reqs[src]);
+        }
+    }
+
+    // Attendi tutte le comunicazioni
+    for (int dest = 0; dest < size; dest++) {
+        if (dest != rank) {
+            MPI_Wait(&send_reqs[dest], &send_statuses[dest]);
+            MPI_Wait(&recv_reqs[dest], &recv_statuses[dest]);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     int rank, size;
-    
-    MPI_Init(&argc, &argv);   // Inizializza MPI
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Ottiene il rank (ID) del processo
-    MPI_Comm_size(MPI_COMM_WORLD, &size);  // Ottiene il numero totale di processi
-    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Chiamata alla barriera personalizzata
     printf("Process %d: Before Barrier\n", rank);
-    
-    MPI_Barrier(MPI_COMM_WORLD);  // Barriera: tutti i processi attendono qui
-    
+    barrier(rank, size);
     printf("Process %d: After Barrier\n", rank);
-    
-    MPI_Finalize();  // Finalizza MPI
-    
+
+    MPI_Finalize();
     return 0;
 }
--- Laboratorio/OpenMP/Codici/Ordered_Barrier.cpp
+++ Laboratorio/OpenMP/Codici/Ordered_Barrier.cpp
@@ -1,29 +1,58 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+#include <unistd.h>
+
+#define maxsize 3000
+
+MPI_Request req;
+MPI_Status status;
+int cont, buffer[maxsize];
+
+void barrier(int rank, int size) {
+    cont++;
+    
+    // Invia token a tutti gli altri processi
+    for (int dest = 0; dest < size; dest++) {
+        if (dest != rank) {
+            MPI_Isend(&buffer[0], size, MPI_INT, dest, cont, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+        }
+
+    }
+
+    // Ricevi token da tutti gli altri processi
+    for (int src = 0; src < size; src++) {
+        if (src != rank) {
+            MPI_Irecv(&buffer[0], size, MPI_INT, src, cont, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+        }
+    }
+
+}
 
 int main(int argc, char *argv[]) {
     int rank, size;
-    
-    MPI_Init(&argc, &argv);   // Inizializza MPI
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Ottiene il rank (ID) del processo
-    MPI_Comm_size(MPI_COMM_WORLD, &size);  // Ottiene il numero totale di processi
-    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     for (int i = 0; i < size; i++) {
         if (rank == i) {
             printf("Process %d: Before Barrier\n", rank);
+            fflush(stdout);
         }
-        MPI_Barrier(MPI_COMM_WORLD);  // Barriera: tutti i processi attendono qui
+        barrier(rank, size);  // Barriera: tutti i processi attendono qui
     }
     
     for (int i = 0; i < size; i++) {
-        MPI_Barrier(MPI_COMM_WORLD);  // Barriera: tutti i processi attendono qui
+        barrier(rank, size);  // Barriera: tutti i processi attendono qui
         if (rank == i) {
             printf("Process %d: After Barrier\n", rank);
+            fflush(stdout);
         }
     }
     
-    MPI_Finalize();  // Finalizza MPI
-    
+    MPI_Finalize();
     return 0;
 }
