4ec5aad52a5d54882dbefd63a8183091512688cc
Rhaegal222 <rhaegal222@gmail.com>
2023-09-09 09:46:26 +0200

    gol commentato

--- Laboratorio/Allegro (installazione, file prova, problemi, etc)/game of life/main.cpp
+++ Laboratorio/Allegro (installazione, file prova, problemi, etc)/game of life/main.cpp
@@ -8,35 +8,42 @@
 using namespace std;
 using namespace jallegro;
 
-int dim=50;
-Panel* p;
+int dim = 50;  // Dimensione della griglia
+Panel* p;      // Puntatore a un oggetto Panel
 
-bool analyzeMatrix(int, int, int** );
-void game(int**,int**);
+// Dichiarazioni delle funzioni
+bool analyzeMatrix(int, int, int**);
+void game(int**, int**);
 void print();
 
-int main(int argc, char **argv) {
-    Allegro::init();
+int main(int argc, char** argv) {
+    Allegro::init();  // Inizializza Allegro
 
-    srand(time(0));
-    int **mat1 = new int* [dim];
-    int **mat2 = new int* [dim];
+    srand(time(0));  // Inizializza il generatore di numeri casuali
 
-    for (int i=0;i<dim;++i) {
+    // Alloca memoria per due matrici bidimensionali di dimensione 'dim'
+    int** mat1 = new int*[dim];
+    int** mat2 = new int*[dim];
+
+    // Inizializza mat1 con valori casuali (0 o 1)
+    for (int i = 0; i < dim; ++i) {
         mat1[i] = new int[dim];
         mat2[i] = new int[dim];
-        for (int j=0;j<dim;++j)
-            mat1[i][j]=rand()%2;
+        for (int j = 0; j < dim; ++j)
+            mat1[i][j] = rand() % 2;
     }
 
-    //Code for showing a window and a panel
+    // Crea una finestra Allegro e un pannello per visualizzare il gioco
     Frame f(dim * 10 + 20, dim * 10 + 20, "Game of life");
     p = new GameOfLifePanel(mat1, dim, dim);
     f.add(p);
     f.show();
+
+    // Avvia il gioco
     game(mat1, mat2);
 
-    for (int i=0;i<dim;++i) {
+    // Libera la memoria allocata
+    for (int i = 0; i < dim; ++i) {
         delete[] mat1[i];
         delete[] mat2[i];
     }
@@ -46,30 +53,35 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+// Implementazione della funzione 'game'
 void game(int** mainMatrix, int** auxMatrix) {
     int time = 0;
     int time_limit = 1000;
-    while(time < time_limit) {
+    while (time < time_limit) {
         time++;
-        print();
-        Allegro::sleep(0.2);
+        print();  // Visualizza lo stato corrente
+        Allegro::sleep(0.2);  // Pausa di 0.2 secondi
+
+        // Calcola il prossimo stato del gioco
         for (int i = 0; i < dim; ++i) {
             for (int j = 0; j < dim; ++j) {
                 auxMatrix[i][j] = analyzeMatrix(i, j, mainMatrix);
             }
         }
-        //Swap mainMatrix and auxMatrix
+
+        // Scambia mainMatrix e auxMatrix
         int** tmp = mainMatrix;
         mainMatrix = auxMatrix;
         auxMatrix = tmp;
     }
 }
 
+// Implementazione della funzione 'analyzeMatrix'
 bool analyzeMatrix(int x, int y, int** mainMatrix) {
     int countAlive = 0;
-    for (int i=x-1;i<=x+1;++i) {
-        for (int j=y-1;j<=y+1;++j) {
-            if (i>=0 && i<dim && j>=0 && j<dim) {
+    for (int i = x - 1; i <= x + 1; ++i) {
+        for (int j = y - 1; j <= y + 1; ++j) {
+            if (i >= 0 && i < dim && j >= 0 && j < dim) {
                 if (mainMatrix[i][j] == 1)
                     countAlive++;
             }
@@ -78,16 +90,16 @@ bool analyzeMatrix(int x, int y, int** mainMatrix) {
     if (mainMatrix[x][y] == 1) {
         countAlive--;
         if (countAlive > 3 || countAlive < 2)
-            return 0;
-        return 1;
-    }
-    else  {
+            return 0;  // La cella muore
+        return 1;      // La cella sopravvive
+    } else {
         if (countAlive == 3)
-            return 1;
-        return 0;
+            return 1;  // Una nuova cella viene generata
+        return 0;      // La cella rimane morta
     }
 }
 
+// Implementazione della funzione 'print'
 void print() {
-    p->repaint();
+    p->repaint();  // Ridisegna il pannello di gioco
 }
